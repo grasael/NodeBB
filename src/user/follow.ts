cf9b851fede346e27b96f435f9077eac47802acc
grasael <gracelia@andrew.cmu.edu>
2023-09-06 13:19:45 -0400

    Added User interface

--- src/user/follow.ts
+++ src/user/follow.ts
@@ -3,15 +3,18 @@ import * as plugins from '../plugins';
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call;
 import * as db from '../database';
 
-module.exports = function (User) {
-    User.follow = async function (uid : string, followuid : string) {
-        await toggleFollow('follow', uid, followuid);
-    };
+interface User {
+    follow(uid: string, followuid: string): Promise<void>;
+    unfollow(uid: string, unfollowuid: string): Promise<void>;
+    getFollowing(uid: string, start: number, stop: number): Promise<User[]>; // Adjust the return type
+    getFollowers(uid: string, start: number, stop: number): Promise<User[]>; // Adjust the return type
+    isFollowing(uid: string, theirid: string): Promise<boolean>;
+    exists(theiruid: string): Promise<boolean>; // Add the 'exists' method
+    setUserField(uid: string, field: string, value: string): Promise<void>; // Add the 'setUserField' method
+    getUsers(uids: string[], requesterUid: string): Promise<User[]>;
+}
 
-    User.unfollow = async function (uid : string, unfollowuid : string) {
-        await toggleFollow('unfollow', uid, unfollowuid);
-    };
-
+module.exports = function (user : User) {
     async function toggleFollow(type : string, uid : string, theiruid : string) {
         if (parseInt(uid, 10) <= 0 || parseInt(theiruid, 10) <= 0) {
             throw new Error('[[error:invalid-uid]]');
@@ -20,11 +23,11 @@ module.exports = function (User) {
         if (parseInt(uid, 10) === parseInt(theiruid, 10)) {
             throw new Error('[[error:you-cant-follow-yourself]]');
         }
-        const exists = await User.exists(theiruid);
+        const exists = await user.exists(theiruid);
         if (!exists) {
             throw new Error('[[error:no-user]]');
         }
-        const isFollowing = await User.isFollowing(uid, theiruid);
+        const isFollowing = await user.isFollowing(uid, theiruid);
         if (type === 'follow') {
             if (isFollowing) {
                 throw new Error('[[error:already-following]]');
@@ -53,18 +56,10 @@ module.exports = function (User) {
             db.sortedSetCard(`followers:${theiruid}`),
         ]);
         await Promise.all([
-            User.setUserField(uid, 'followingCount', followingCount),
-            User.setUserField(theiruid, 'followerCount', followerCount),
+            user.setUserField(uid, 'followingCount', followingCount),
+            user.setUserField(theiruid, 'followerCount', followerCount),
         ]);
     }
-
-    User.getFollowing = async function (uid : string, start : number, stop : number) {
-        return await getFollow(uid, 'following', start, stop);
-    };
-
-    User.getFollowers = async function (uid : string, start : number, stop : number) {
-        return await getFollow(uid, 'followers', start, stop);
-    };
 
     async function getFollow(uid : string, type : string, start : number, stop : number) {
         if (parseInt(uid, 10) <= 0) {
@@ -77,10 +72,26 @@ module.exports = function (User) {
             start: start,
             stop: stop,
         });
-        return await User.getUsers(data.uids, uid);
+        return await user.getUsers(data.uids, uid);
     }
+    
+    user.follow = async function (uid : string, followuid : string) {
+        await toggleFollow('follow', uid, followuid);
+    };
 
-    User.isFollowing = async function (uid : string, theirid : string) {
+    user.unfollow = async function (uid : string, unfollowuid : string) {
+        await toggleFollow('unfollow', uid, unfollowuid);
+    };
+
+    user.getFollowing = async function (uid : string, start : number, stop : number) {
+        return await getFollow(uid, 'following', start, stop);
+    };
+
+    user.getFollowers = async function (uid : string, start : number, stop : number) {
+        return await getFollow(uid, 'followers', start, stop);
+    };
+
+    user.isFollowing = async function (uid : string, theirid : string) {
         if (parseInt(uid, 10) <= 0 || parseInt(theirid, 10) <= 0) {
             return false;
         }
